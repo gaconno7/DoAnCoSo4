a21ffe8c4f806080deb8804f51bd5b535ee0a7a2
gaconno7 <tranthehuy2002@gmail.com>
2022-12-14 15:14:25 +0700

    add file

--- src/main/java/vn/com/stanford/springmvc/controller/FrontController.java
+++ src/main/java/vn/com/stanford/springmvc/controller/FrontController.java
@@ -7,13 +7,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontController {
 
+    @RequestMapping(value = "/join")
+    public String showJoin(Model model) {
+        return "join";
+    }
+
+    @RequestMapping(value = "/test-info")
+    public String showTestJoin(Model model) {
+        return "test-info";
+    }
+
+    @RequestMapping(value = "/test")
+    public String showTest(Model model) {
+        return "test";
+    }
+
     @RequestMapping(value = "/signin")
     public String showSignin(Model model) {
         return "signin";
     }
 
-    @RequestMapping(value = "/join")
-    public String showJoin(Model model) {
-        return "join";
+    @RequestMapping(value = "/signup")
+    public String showSignup(Model model) {
+        return "signup";
     }
+
+
 }
